e78411e5eb4cbcdfbeffe304d3cc33a5fb8a8eb0
Dmitry Burlakov <hi@dantaeusb.me>
2020-06-18 03:13:12 +0300

    Faker library and usage

--- app/code/Dantaeusb/TrustedCustomer/Model/ResourceModel/CustomerRepository.php
+++ app/code/Dantaeusb/TrustedCustomer/Model/ResourceModel/CustomerRepository.php
@@ -3,18 +3,13 @@
 
 namespace Dantaeusb\TrustedCustomer\Model\ResourceModel;
 
-use Dantaeusb\TrustedCustomer\Setup\InstallData;
-use Magento\Customer\Api\Data\CustomerSearchResultsInterface;
-use Magento\Customer\Model\Customer;
-use Magento\Customer\Model\CustomerFactory;
-use Dantaeusb\TrustedCustomer\Api\TrustedCustomerRepositoryInterface;
-use Magento\Framework\Webapi\Exception;
-
 class CustomerRepository
 {
     public function afterGetById($customerId, \Magento\Customer\Api\Data\CustomerInterface $result)
     {
-        $result->setEmail();
+        $faker = \Faker\Factory::create();
+
+        $result->setEmail($faker->email);
 
         return $result;
     }
